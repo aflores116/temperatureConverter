5e31fbd74a1305a87362ddc174a76e226b695996
Angel Flores <a_flores116@yahoo.com>
2018-10-03 21:56:55 -0500

    installed packages

---
+++ temperatureConverter/tempconversion.js
@@ -0,0 +1,9 @@
+let fahrenheit = 50; // 0c - 273.15k
+
+let celsius = (fahrenheit - 32) * 5/9;
+let toKelvin = (fahrenheit + 459.67) * 5/9;
+let kelvin = toKelvin + "k";
+
+
+console.log(celsius);
+console.log(kelvin);
